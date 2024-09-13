2cd5ea07a65678331854ff698ccc5bfe96674566
niiidai <danny414dn@gmail.com>
2024-09-12 23:20:17 -0700

    initial docker

---
+++ provision.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+echo 'Start!'
+
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.6 2
+
+cd /vagrant
+
+sudo apt-get update
+sudo apt-get install tree
+
+# install and configure mysql8
+if ! [ -e /vagrant/mysql-apt-config_0.8.15-1_all.deb ]; then
+	wget -c https://dev.mysql.com/get/mysql-apt-config_0.8.15-1_all.deb
+fi
+
+sudo dpkg -i mysql-apt-config_0.8.15-1_all.deb
+sudo DEBIAN_FRONTEND=noninteractivate apt-get install -y mysql-server
+sudo apt-get install -y libmysqlclient-dev
+
+if [ ! -f "/usr/bin/pip" ]; then
+  sudo apt-get install -y python3-pip
+  sudo apt-get install -y python-setuptools
+  sudo ln -s /usr/bin/pip3 /usr/bin/pip
+else
+  echo "pip3 installed"
+fi
+
+# upgrade pip
+# python -m pip install --upgrade pip
+pip install --upgrade setuptools
+pip install --ignore-installed wrapt
+# install the latest pip
+pip install -U pip
+# pip install packages in requirements.txt
+pip install -r requirements.txt
+
+
+# set mysql root passwd to yourpassword
+# create a database named twitter
+sudo mysql -u root << EOF
+	ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'yourpassword';
+	flush privileges;
+	show databases;
+	CREATE DATABASE IF NOT EXISTS twitter;
+EOF
+# fi
+
+echo 'All Done!'
