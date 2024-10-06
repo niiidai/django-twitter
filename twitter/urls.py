1f10e3970bde524f9800a01f1bb4e3ffe658f018
niiidai <danny414dn@gmail.com>
2024-10-05 18:22:26 -0700

    Implement Comment Create API and Unit Tests

--- twitter/urls.py
+++ twitter/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 from tweets.api.views import TweetViewSet
 from friendships.api.views import FriendshipViewSet
 from newsfeeds.api.views import NewsFeedViewSet
+from comments.api.views import CommentViewSet
 
 router = routers.DefaultRouter()
 router.register(r'api/users', views.UserViewSet)
@@ -28,6 +29,7 @@ router.register(r'api/accounts', views.AccountViewSet, basename='accounts')
 router.register(r'api/tweets', TweetViewSet, basename='tweets')
 router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
+router.register(r'api/comments', CommentViewSet, basename='comments')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
