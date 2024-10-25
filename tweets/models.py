76143e7fc7723bf975584f1126a4f696ea22673c
niiidai <danny414dn@gmail.com>
2024-10-25 10:06:35 -0700

    Add Like Model

--- tweets/models.py
+++ tweets/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from utils.time_helpers import utc_now
+from likes.models import Like
+from django.contrib.contenttypes.models import ContentType
 
 class Tweet(models.Model):
     user = models.ForeignKey(
@@ -16,12 +18,20 @@ class Tweet(models.Model):
         index_together = (('user', 'created_at'),)
         ordering = ('user', '-created_at',)
 
+    def __str__(self):
+        # will show tweet contents when run print(tweet instance) in python manage.py shell
+        return f"{self.created_at} {self.user}: {self.content}"
+
     @property
     def hours_to_now(self):
         # datetime.now() does not contain time zone info,
         # therefore we can't use datetime.now() minus self.created_at
         return (utc_now() - self.created_at).seconds // (60 * 60)
 
-    def __str__(self):
-        # will show tweet contents when run print(tweet instance) in python manage.py shell
-        return f"{self.created_at} {self.user}: {self.content}"
+    @property
+    def like_set(self):
+        return Like.objects.filter(
+            content_type=ContentType.objects.get_for_model(Tweet),
+            object_id=self.id,
+        ).order_by('-created_at')
+
--- tweets/tests.py
+++ tweets/tests.py
@@ -1,13 +1,25 @@
-from django.test import TestCase
-from django.contrib.auth.models import User
-from tweets.models import Tweet
 from datetime import timedelta
 from utils.time_helpers import utc_now
+from testing.testcases import TestCase
 
-class TweetTestCase(TestCase):
+
+class TweetTests(TestCase):
+    def setUp(self):
+        self.user1 = self.create_user('user1')
+        self.tweet = self.create_tweet(self.user1, content = "I'm Hanpi.")
+
     def test_hours_to_now(self):
-        nina = User.objects.create_user(username='nina')
-        tweet = Tweet.objects.create(user=nina, content="I'm Hanpi.")
-        tweet.created_at = utc_now() - timedelta(hours=10)
-        tweet.save()
-        self.assertEqual(tweet.hours_to_now, 10)
+        self.tweet.created_at = utc_now() - timedelta(hours=10)
+        self.tweet.save()
+        self.assertEqual(self.tweet.hours_to_now, 10)
+
+    def test_like_set(self):
+        self.create_like(self.user1, self.tweet)
+        self.assertEqual(self.tweet.like_set.count(), 1)
+
+        self.create_like(self.user1, self.tweet)
+        self.assertEqual(self.tweet.like_set.count(), 1)
+
+        user2 = self.create_user('user2')
+        self.create_like(user2, self.tweet)
+        self.assertEqual(self.tweet.like_set.count(), 2)
