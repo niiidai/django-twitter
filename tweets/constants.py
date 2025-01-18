1775696fc8d5ff8d99c012f34e63a52e24946ee7
niiidai <danny414dn@gmail.com>
2025-01-18 14:54:04 -0800

    Add TweetPhoto API

--- tweets/constants.py
+++ tweets/constants.py
@@ -4,9 +4,12 @@ class TweetPhotoStatus:
     REJECTED = 2
 
 
-
 TWEET_PHOTO_STATUS_CHOICES = (
     (TweetPhotoStatus.PENDING, 'Pending'),
     (TweetPhotoStatus.APPROVED, 'Approved'),
     (TweetPhotoStatus.REJECTED, 'Rejected'),
 )
+
+
+TWEET_PHOTOS_UPLOAD_LIMIT = 9
+
