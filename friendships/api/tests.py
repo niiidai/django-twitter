4bcd48c5c4b7707ea9d720642bf1b79b1b6facf2
niiidai <danny414dn@gmail.com>
2025-01-20 20:23:36 -0800

    Add Friendship Pagination

--- friendships/api/tests.py
+++ friendships/api/tests.py
@@ -1,11 +1,12 @@
 from friendships.models import Friendship
-from rest_framework.test import APIClient
 from testing.testcases import TestCase
+from utils.paginations import FriendshipPagination
 
 FOLLOW_URL = '/api/friendships/{}/follow/'
 UNFOLLOW_URL = '/api/friendships/{}/unfollow/'
 FOLLOWERS_URL = '/api/friendships/{}/followers/'
 FOLLOWINGS_URL = '/api/friendships/{}/followings/'
+
 
 class FriendshipApiTests(TestCase):
 
@@ -86,23 +87,23 @@ class FriendshipApiTests(TestCase):
         # GET request is okay
         response = self.anonymous_client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data['following']), 3)
+        self.assertEqual(len(response.data['results']), 3)
         # make sure the info is ordered by "-created_at"
-        ts0 = response.data['following'][0]['created_at']
-        ts1 = response.data['following'][1]['created_at']
-        ts2 = response.data['following'][2]['created_at']
+        ts0 = response.data['results'][0]['created_at']
+        ts1 = response.data['results'][1]['created_at']
+        ts2 = response.data['results'][2]['created_at']
         self.assertEqual(ts0 > ts1, True)
         self.assertEqual(ts1 > ts2, True)
         self.assertEqual(
-            response.data['following'][0]['user']['username'],
+            response.data['results'][0]['user']['username'],
             'user2_following2',
         )
         self.assertEqual(
-            response.data['following'][1]['user']['username'],
+            response.data['results'][1]['user']['username'],
             'user2_following1',
         )
         self.assertEqual(
-            response.data['following'][2]['user']['username'],
+            response.data['results'][2]['user']['username'],
             'user2_following0',
         )
 
@@ -114,16 +115,104 @@ class FriendshipApiTests(TestCase):
         # GET is okay
         response = self.anonymous_client.get(url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data['followers']), 2)
+        self.assertEqual(len(response.data['results']), 2)
         # make sure the info is ordered by "-created_at"
-        ts0 = response.data['followers'][0]['created_at']
-        ts1 = response.data['followers'][1]['created_at']
+        ts0 = response.data['results'][0]['created_at']
+        ts1 = response.data['results'][1]['created_at']
         self.assertEqual(ts0 > ts1, True)
         self.assertEqual(
-            response.data['followers'][0]['user']['username'],
+            response.data['results'][0]['user']['username'],
             'user2_follower1',
         )
         self.assertEqual(
-            response.data['followers'][1]['user']['username'],
+            response.data['results'][1]['user']['username'],
             'user2_follower0',
         )
+
+    def test_followers_pagination(self):
+        max_page_size = FriendshipPagination.max_page_size
+        page_size = FriendshipPagination.page_size
+        for i in range(page_size * 2):
+            follower = self.create_user('user1_follower{}'.format(i))
+            Friendship.objects.create(from_user=follower, to_user=self.user1)
+            if follower.id % 2 == 0:
+                Friendship.objects.create(from_user=self.user2, to_user=follower)
+
+        url = FOLLOWERS_URL.format(self.user1.id)
+        self._test_friendship_pagination(url, page_size, max_page_size)
+
+        # anonymous hasn't followed any users
+        response = self.anonymous_client.get(url, {'page': 1})
+        for result in response.data['results']:
+            self.assertEqual(result['has_followed'], False)
+
+        # user2 has followed users with even id
+        response = self.user2_client.get(url, {'page': 1})
+        for result in response.data['results']:
+            has_followed = (result['user']['id'] % 2 == 0)
+            self.assertEqual(result['has_followed'], has_followed)
+
+    def test_followings_pagination(self):
+        max_page_size = FriendshipPagination.max_page_size
+        page_size = FriendshipPagination.page_size
+        for i in range(page_size * 2):
+            following = self.create_user('user1_following{}'.format(i))
+            Friendship.objects.create(from_user=self.user1, to_user=following)
+            if following.id % 2 == 0:
+                Friendship.objects.create(from_user=self.user2, to_user=following)
+
+        url = FOLLOWINGS_URL.format(self.user1.id)
+        self._test_friendship_pagination(url, page_size, max_page_size)
+
+        # Anonymous hasn't followed any users
+        response = self.anonymous_client.get(url, {'page': 1})
+        for result in response.data['results']:
+            self.assertEqual(result['has_followed'], False)
+
+        # user2 has followed users with even id
+        response = self.user2_client.get(url, {'page': 1})
+        for result in response.data['results']:
+            has_followed = (result['user']['id'] % 2 == 0)
+            self.assertEqual(result['has_followed'], has_followed)
+
+        # user1 has followed all his following users
+        response = self.user1_client.get(url, {'page': 1})
+        for result in response.data['results']:
+            self.assertEqual(result['has_followed'], True)
+
+    def _test_friendship_pagination(self, url, page_size, max_page_size):
+        response = self.anonymous_client.get(url, {'page': 1})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['results']), page_size)
+        self.assertEqual(response.data['total_pages'], 2)
+        self.assertEqual(response.data['total_results'], page_size * 2)
+        self.assertEqual(response.data['page_number'], 1)
+        self.assertEqual(response.data['has_next_page'], True)
+
+        response = self.anonymous_client.get(url, {'page': 2})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.data['results']), page_size)
+        self.assertEqual(response.data['total_pages'], 2)
+        self.assertEqual(response.data['total_results'], page_size * 2)
+        self.assertEqual(response.data['page_number'], 2)
+        self.assertEqual(response.data['has_next_page'], False)
+
+        response = self.anonymous_client.get(url, {'page': 3})
+        self.assertEqual(response.status_code, 404)
+
+        # Test user cannot customize page_size exceeds max_page_size
+        response = self.anonymous_client.get(url, {'page': 1, 'size': max_page_size + 1})
+        self.assertEqual(len(response.data['results']), max_page_size)
+        self.assertEqual(response.data['total_pages'], 2)
+        self.assertEqual(response.data['total_results'], page_size * 2)
+        self.assertEqual(response.data['page_number'], 1)
+        self.assertEqual(response.data['has_next_page'], True)
+
+        # Test user can customize page size by param size
+        response = self.anonymous_client.get(url, {'page': 1, 'size': 2})
+        self.assertEqual(len(response.data['results']), 2)
+        self.assertEqual(response.data['total_pages'], page_size)
+        self.assertEqual(response.data['total_results'], page_size * 2)
+        self.assertEqual(response.data['page_number'], 1)
+        self.assertEqual(response.data['has_next_page'], True)
+
--- friendships/api/views.py
+++ friendships/api/views.py
@@ -1,18 +1,21 @@
-from rest_framework import viewsets, status
-from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.response import Response
-from friendships.models import Friendship
+from django.contrib.auth.models import User
 from friendships.api.serializers import (
     FollowingSerializer,
     FollowerSerializer,
     FriendshipCreateSerializer
 )
-from django.contrib.auth.models import User
+from friendships.models import Friendship
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from utils.paginations import FriendshipPagination
 
 class FriendshipViewSet(viewsets.GenericViewSet):
     serializer_class = FriendshipCreateSerializer
     queryset = User.objects.all()
+    # Different views may need different pagination settings.
+    pagination_class = FriendshipPagination
 
     # We would like to use POST /api/friendships/1/follow
     # to follow the user with user_id = 1.
@@ -22,20 +25,16 @@ class FriendshipViewSet(viewsets.GenericViewSet):
     def followers(self, request, pk):
         # GET /api/friendships/1/followers/
         friendships = Friendship.objects.filter(to_user_id=pk).order_by('-created_at')
-        serializer = FollowerSerializer(friendships, many=True)
-        return Response(
-            {'followers': serializer.data},
-            status=status.HTTP_200_OK,
-        )
+        page = self.paginate_queryset(friendships)
+        serializer = FollowerSerializer(page, many=True, context={'request': request})
+        return self.get_paginated_response(serializer.data)
 
     @action(methods=['GET'], detail=True, permission_classes=[AllowAny])
     def followings(self, request, pk):
         friendships = Friendship.objects.filter(from_user_id=pk).order_by('-created_at')
-        serializer = FollowingSerializer(friendships, many=True)
-        return Response(
-            {'following': serializer.data},
-            status=status.HTTP_200_OK,
-        )
+        page = self.paginate_queryset(friendships)
+        serializer = FollowingSerializer(page, many=True, context={'request': request})
+        return self.get_paginated_response(serializer.data)
 
     @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
     def follow(self, request, pk):
@@ -56,7 +55,7 @@ class FriendshipViewSet(viewsets.GenericViewSet):
             }, status=status.HTTP_400_BAD_REQUEST)
         instance = serializer.save()
         return Response(
-            FollowingSerializer(instance).data,
+            FollowingSerializer(instance, context={'request': request}).data,
             status=status.HTTP_201_CREATED,
         )
 
